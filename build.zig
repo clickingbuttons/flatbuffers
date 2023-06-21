0c2cbf3077fa9f24618cdfcfe3a72fdf7c31f17b
clickingbuttons <clickingbuttons@pm.me>
2023-06-21 16:35:30 -0400

    add build.zig

---
+++ build.zig
@@ -0,0 +1,53 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const cpp_flags = [_][]const u8{
+        "-Wall",
+        "-Werror",
+        "-fno-rtti",
+        "-pedantic",
+        "-Wextra",
+        "-Wno-unused-parameter",
+        "-Wold-style-cast",
+        "-fsigned-char",
+        "-Wnon-virtual-dtor",
+        "-Wunused-result",
+        "-Wunused-parameter",
+        "-Werror=unused-parameter",
+        "-Wmissing-declarations",
+        "-Wzero-as-null-pointer-constant",
+        "-faligned-new",
+        "-Wextra-semi",
+    };
+
+    const flatc = b.addExecutable(.{
+        .name = "flatc",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    flatc.addCSourceFiles(&.{
+        "src/idl_parser.cpp",
+        "src/idl_gen_binary.cpp",
+        "src/idl_gen_text.cpp",
+        "src/reflection.cpp",
+        "src/util.cpp",
+        "src/flatc.cpp",
+        "src/flatc_main.cpp",
+        "src/binary_annotator.cpp",
+        "src/annotated_binary_text_gen.cpp",
+        "src/bfbs_gen_lua.cpp",
+        "src/bfbs_gen_nim.cpp",
+    }, &cpp_flags);
+
+    for ([_][]const u8{
+        "include",
+        "flatbuffers/include",
+    }) |include_path| flatc.addIncludePath(include_path);
+
+    flatc.linkLibCpp();
+    b.installArtifact(flatc);
+}
